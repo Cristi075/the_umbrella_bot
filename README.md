1506f8e2af2ba1a17e667725edd8068f0eda7bdb
Cristi075 <1867383+Cristi075@users.noreply.github.com>
2023-06-09 23:19:14 +0300

    Initial commit

--- README.md
+++ README.md
@@ -4,19 +4,70 @@
 
 Use this template for the genezio challenge at Techsylvania 2023.
 
-# Tool Name
+# The Umbrella Bot
+
+Reminds you to take your umbrella with you in the morning (if it is going to rain).
 
 ## Previous Workflow
 
-TODO - Describe how the process was managed before using your tool. Specify how many hours can be saved by using this project.
+Usually, you would check the weather app to see if it is going to rain later in the day when leaving home in the morning.  
+But what if you forget to do that exactly on the day when it rains ...
+
+What if you could get this kind of information in your chat app every morning?
+![Screenshot demonstrating functionality](./poc_screenshot.png)
 
 ## Solution Description
 
-TODO - Provide a brief overview of the tool, explaining its purpose and functionality.
+This tool will query a weather API (openweathermap) every morning at 6 AM, check if it is going to rain and then it notifies you using Discord (or other chat platform that you like, but this implementation uses Discord).
+
+The notifications will mention whether or not you should bring an umbrella with you today.  
+It will also tell you the minimum and maximum temperature for hat day.  
+
+Use this and never forget your umbrella at home again 🌞
+
 
 ## Deployment
 
-TODO - Deploy your full-stack solution using [genezio](https://genez.io/). You can use [these examples](https://github.com/Genez-io/genezio-examples) as a starting point.
+Since this relies on Discord in order to get the information to you, you will have to do some setup before deploying your own Umbrella Bot.  
+Don't worry, it's not complicated.  
 
-TODO - Provide a link to your live app so we can test it out.
+### Configuring Discord webhooks
+- Go to your Discord server, and right click on it and select Server Settings -> Integrations  
+- Click on webhooks
+- Click on 'New Webhook'
+- Click on the newly created webhook, give your Webhook Bot a name and chose a channel.
+- Click 'Copy Webhook URL' and paste that value somewhere.
 
+### Get your OpenWeatherMap API key
+
+Go to [https://openweathermap.org/](https://openweathermap.org/) and sign up. You will have to confirm your email and then you'll get your API key quickly.  
+After logging in, go to [https://home.openweathermap.org/api_keys](https://home.openweathermap.org/api_keys) and get your API key.
+
+### Preparing your .env file
+
+Create a .env file in this project and populate it with these four values:
+```
+WEBHOOK_URL=<webhook_url>
+LATITUDE=46.821
+LONGITUDE=23.582
+OWM_API_KEY=<api_key>
+```
+
+Replace <webhook_url> with the URL that you copied before (from Discord).  
+Replace <api_key> with your API from the last step.  
+And replace LATITUDE and LONGITUDE with some coordinates close to your home or to your office.
+
+### Deployment time!
+
+You are now ready to deploy your bot!
+
+This tool is deployed by using [genezio](https://genez.io/).  
+You must have the CLI tool installed and you have to be logged in.  
+After everything is ready, just run ``` genezio deploy```.
+
+The cronstring ("0 6 * * *") from genezio.yml makes the bot send a message at 6 AM.  
+You can change that by replacing the cronstring. Use [https://crontab.guru](https://crontab.guru) as a  reference.
+
+### Local testing
+
+If you want to test your bot locally first, instead of running ``` genezio deploy ```, you can run ``` genezio local```
---
+++ main.ts
@@ -0,0 +1,129 @@
+import { Logger, ILogObj } from "tslog";
+import axios from 'axios';
+import 'dotenv/config';
+
+const logger: Logger<ILogObj> = new Logger();
+
+const WEATHER_URL = (lat: string, lon: string, apiKey: string) => `https://api.openweathermap.org/data/2.5/forecast?units=metric&lat=${lat}&lon=${lon}&appid=${apiKey}`
+
+// Based on https://openweathermap.org/forecast5
+interface WeatherForecast {
+    dt: Date;
+    main: {
+        temp: number;
+        feels_like: number;
+        temp_min: number;
+        temp_max: number;
+        humidity: number;
+    };
+    weather: {
+        main: string;
+        description: string;
+    }[];
+    pop: number; // Probability of precipitation
+    dt_txt: string;
+}
+
+// Based on https://openweathermap.org/forecast5
+interface WeatherApiResponse {
+    cnt: number;
+    list: WeatherForecast[];
+}
+
+export class UmbrellaBotService {
+
+    // The main loop of the program, this is the method that will get called on a schedule
+    public async main() {
+
+        const forecasts = await this.getWeatherData();
+
+        const rainDetected = forecasts.filter(f => f.weather[0].main.toLowerCase().indexOf('rain')>=0);
+        logger.info(`Rain forecasted at ${rainDetected.length} moments`);
+        const maxProbability = rainDetected.map(f=>f.pop).reduce((a, b) => Math.max(a, b), -Infinity);
+        const maxProbabilityTimes = rainDetected.filter(f=> f.pop == maxProbability).map(f => f.dt_txt);
+        logger.info(`Probability of rain: ${maxProbability}. At: ${maxProbabilityTimes}`)
+
+        const maxTemperature = forecasts.map(f=>f.main.temp_max).reduce((a, b) => Math.max(a, b), -Infinity);
+        const minTemperature = forecasts.map(f=>f.main.temp_min).reduce((a, b) => Math.min(a, b), +Infinity);
+
+        let message: string = 'Good morning!\n\n';
+        if (rainDetected.length >= 1){
+            message += 'You should take your umbrella with you. ☔\n';
+            message += 'There is rain forecasted for today. 🌧\n';
+            message += `The highest probability of rain is ${maxProbability*100}%.\n`;
+            message += `That is forecasted for ${maxProbabilityTimes}.\n`;
+        } else {
+            message += 'You can leave your umbrella at home today! 🌞\n';
+            message += `It looks like it isn't going to rain.\n`
+        }
+        message += `\nThe temperature is going to be between ${minTemperature}°C and ${maxTemperature}°C.\n`
+        message += '\nHave a nice day!'
+
+        this.sendWebhookMessage(message);
+    }
+
+
+    // Use this method to send a webhook-based message
+    // This implementation uses Discord, but it is kept platform-agnostic since it doesn't use any
+    // Discord-specific features like embeds
+    // This means that you can easily adapt it to work with your messaging platform of choice
+    private async sendWebhookMessage(message: string) {
+        const webhook_url: string | undefined = process.env.WEBHOOK_URL;
+        if (!webhook_url) {
+            logger.error(`Invalid webhook URL: ${webhook_url}`)
+            return;
+        }
+
+        const { data, status } = await axios.post<any>(
+            webhook_url,
+            {
+                content: message
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
+            }
+        );
+
+        const response_body: string = JSON.stringify(data);
+        logger.info(`Discord webhook response. Status code = ${status}. Response body = ${response_body}`);
+    }
+
+    // Returns the forecasts that are relevant for predicting today's weather
+    // This assumes that the call is going to be made in the morning
+    // A possible improvement would be to check each timestamp and only return those within the current day
+    private async getWeatherData() {
+        const latitude: string | undefined = process.env.LATITUDE;
+        const longitude: string | undefined = process.env.LONGITUDE;
+        const apiKey: string | undefined = process.env.OWM_API_KEY;
+        if (!latitude || !longitude || !apiKey) {
+            logger.error('Invalid environment vars: latitude/longitude/apiKey');
+            return [];
+        }
+
+        const { data, status } = await axios.get<WeatherApiResponse>(
+            WEATHER_URL(latitude, longitude, apiKey)
+        );
+
+        logger.info(`Weather API response. Status code = ${status}`);
+
+        if (status != 200) {
+            logger.error('Received non-200 status code')
+            return [];
+        }
+
+        const allForecasts: WeatherForecast[] = data.list;
+        if (!allForecasts || allForecasts.length == 0) {
+            logger.error('Received empty response (no forecasts)')
+            return [];
+        }
+
+        // I want this script to be run in the morning (6 or 7 AM)
+        // So only the first 6 forecasts are relevant for today (each forecast is made for a 3h interval)
+        const forecasts: WeatherForecast[] = allForecasts.splice(0, 6);
+
+        return forecasts;
+    }
+}
